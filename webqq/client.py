c4ff3c617d812d219e723994dd79e0ae4b381454
encorehu <huyoo353@126.com>
2014-05-18 22:14:47 +0800

    添加 check_verify_code , 检查验证码, 得到2个验证码(其中一个可能要用户输入)

--- webqq/client.py
+++ webqq/client.py
@@ -46,6 +46,128 @@ class WebQQClient(WebBrowser):
             else:
                 return True
 
+    def check_verify_code(self, uin=None, appid=None):
+        login_sig= self.check_login_sig()
+
+        print u'1.开始检查是否需要输入验证码'
+        """
+        算出用户名加密后的字符串
+        """
+        if uin==None:
+            uin=self.uin
+
+        #GET https://ssl.ptlogin2.qq.com/check?uin=2476202050&appid=1003903&js_ver=10079&js_type=0&login_sig=5Vyb8yT91BH5ZVR3AXrb7Cx-v2PuQ3LhXwCM-3MquWEdgXt005k3SjaGaf4jy1TF&u1=http%3A%2F%2Fweb2.qq.com%2Floginproxy.html&r=0.31033543101511896 HTTP/1.1
+
+        verifyURL = 'https://ssl.ptlogin2.qq.com/check?uin=%(uin)s&appid=%(appid)s&js_ver=10079&js_type=0&login_sig=%(login_sig)s&u1=http%%3A%%2F%%2Fweb2.qq.com%%2Floginproxy.html&r=%(randstamp)s' % {'uin':uin, 'appid':WEBQQ_APPID, 'randstamp':random.random(), 'login_sig':login_sig}
+        print 'GET', verifyURL
+        headers={}
+        headers['Connection'] = 'keep-alive'
+        """
+        获取初次加密所需要的一个关键参数值
+        """
+        '''
+        cookies_attrs=[]
+        _keys=[]
+        needed_cookie_names=['uikey','pgv_pvid','pgv_info','chkuin']
+        for index,cookie in enumerate(self.cookiejar):
+            _keys.append(cookie.name)
+            if cookie.domain.endswith('.qq.com') or cookie.domain.endswith('.ptlogin2.qq.com'):
+                #if cookie.name in needed_cookie_names:
+                if ((cookie.value is not None) and
+                    self.cookiejar.non_word_re.search(cookie.value)):
+                    value = self.cookiejar.quote_re.sub(r"\\\1", cookie.value)
+                else:
+                    value = cookie.value
+
+                if cookie.value is None:
+                    cookies_attrs.append(cookie.name)
+                else:
+                    cookies_attrs.append("%s=%s" % (cookie.name, value))
+
+        print cookies_attrs
+        if not 'chkuin' in _keys:
+            cookies_attrs.append("%s=%s" % ('chkuin', self.uin))
+
+        cookies='; '.join(cookies_attrs)
+        print cookies
+        response = self.get(verifyURL, headers = headers, cookies=cookies)
+        '''
+        import cookielib
+        version = 0
+        name='chkuin'
+        value=self.uin
+        port = None
+        port_specified=None
+        domain, domain_specified, domain_initial_dot='ptlogin2.qq.com', None, None
+        path, path_specified = '/',None
+        secure = None
+        expires = None
+        discard = None
+        comment = None
+        comment_url = None
+        rest = {}
+        c = cookielib.Cookie(version,
+                      name, value,
+                      port, port_specified,
+                      domain, domain_specified, domain_initial_dot,
+                      path, path_specified,
+                      secure,
+                      expires,
+                      discard,
+                      comment,
+                      comment_url,
+                      rest)
+        self.cookiejar.set_cookie(c)
+        response = self.get(verifyURL, headers = headers)
+        print response
+
+        """
+        对response的文本进行提取，第一步拆分成["ptui_checkVC('0'", "'!YQL'", " '\\x00\\x00\\x00\\x00\\x95\\x22\\xea\\x8a');"]
+        """
+        # ptui_checkVC('0','!T23','\x00\x00\x00\x00\x01\xe4\xfa\x78');
+        # ptui_checkVC('2','','\x00\x00\x00\x00\x00\x00\x27\x10');
+
+        content=response
+        content = content.split(',')
+        """
+        提取用于生成加密后的密码的验证字符串1——!YQL 长度一般为4, 如果大于4, 说明需要验证码
+        """
+        verify_code1 = content[1][1:-1]
+
+        """
+        提取用于生成加密后的密码的验证字符串2——\x00\x00\x00\x00\x95\x22\xea\x8a
+        是我们QQ号码的16进制形式， QQ号为：1213200914（我的qq），
+        我们把\x00\x00\x00\x00\x48\x4f\xfa\x12中的\\x去掉之后就剩下484ffa12,
+        我们用计算器转换一下这个数为10进制，便是1213200914
+        """
+        verify_code2 = content[2].split("'")[1]
+        print 'verify_code1',verify_code1
+        print 'verify_code2',verify_code2
+
+        """
+        判断是否出现图片验证码，这里为了图方便判断验证码1是否是4位，不是则为出现图片验证码。其实更好的方法是判断ptui_checkVC('0'"，如果是0，则是文字验证，如果是1则为图片验证
+        """
+        if len(verify_code1) > 4:
+            # 获取验证码图片 http://captcha.qq.com/getimage?aid=1003903&&uin=qq号码&vc_type=verify_code2
+            # 这里不处理验证码的问题
+            print 'Your QQ need capcha to login.'
+            with open('./verify.png','wb') as ff:
+                verify_image_url = 'https://ssl.captcha.qq.com/getimage?aid=1003903&&uin=%(uin)s&cap_cd=%(verify_code1)s' % {'uin':self.uin,'verify_code1':verify_code1}
+                ff.write(self.get(verify_image_url))
+
+            import os
+            os.system('verify.png')
+
+            verify_code1 = raw_input('Please input verify code:')
+            verify_code1 = verify_code1.strip()
+        else:
+            print '不需要输入验证码, 可以进行下一步登录QQ应用平台了.'
+
+        print 'verify_code1',verify_code1
+        print 'verify_code2',verify_code2
+        self.verify_code1, self.verify_code2 = (verify_code1, verify_code2)
+        return (verify_code1, verify_code2)
+
     def login(self, username=None, password=None):
         if username and password:
             self.uin      = username
