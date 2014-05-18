7f963a48836a290ddf43ac8a4805df39fd018557
encorehu <huyoo353@126.com>
2014-05-18 22:44:42 +0800

    关掉没用的调试信息

--- webqqbot.py
+++ webqqbot.py
@@ -1,6 +1,8 @@
 from webqq import WebQQClient
 
-webqq_client = WebQQClient()
+webqq_client = WebQQClient(debug=False)
+
 webqq_client.login(username = '12345678',
                   password = '88888888')
+
 webqq_client.run_forever()
