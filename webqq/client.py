bb1e37a6505eb1cdb33f93002ef1e36892370cf9
encorehu <huyoo353@126.com>
2014-05-23 09:21:44 +0800

    添加 heartbeat 心跳函数

--- webqq/client.py
+++ webqq/client.py
@@ -6,6 +6,9 @@ class WebQQClient(object):
     def logout(self):
         return True
 
+    def heartbeat(self):
+        print 'Bom..bong!'
+
     def run_forever(self):
         i=0
         while True:
