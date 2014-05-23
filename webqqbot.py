60411b94ceb65e672ddcd88f72a1b6587cde6afb
encorehu <huyoo353@126.com>
2014-05-23 20:56:15 +0800

    删除空行

--- webqqbot.py
+++ webqqbot.py
@@ -1,8 +1,6 @@
 from webqq import WebQQClient
 
 webqq_client = WebQQClient(debug=False)
-
 webqq_client.login(username = '12345678',
                   password = '88888888')
-
 webqq_client.run_forever()
